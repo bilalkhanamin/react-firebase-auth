89ad82f563c7f4d8aea028b3f13d4d235f08d595
Bilal Amin Khan <42135526+bilalkhanamin@users.noreply.github.com>
2022-01-24 02:49:37 -0700

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2s-BencNMgb6BeDLitm854DkLinvfHVw",
-  authDomain: "auth-development-ce524.firebaseapp.com",
-  projectId: "auth-development-ce524",
-  storageBucket: "auth-development-ce524.appspot.com",
-  messagingSenderId: "423715777465",
-  appId: "1:423715777465:web:e677d6d7e35752abb62468",
+  apiKey: "",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
 };
 
 const app = initializeApp(firebaseConfig);
